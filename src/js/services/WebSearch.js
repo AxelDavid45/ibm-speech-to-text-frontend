fed57f6413d40fc26db53fbff78513efa398a593
Axel Espinosa <axeldavid45@gmail.com>
2020-08-05 15:14:01 -0500

    Modified apikey

--- src/js/services/WebSearch.js
+++ src/js/services/WebSearch.js
@@ -1,4 +1,4 @@
-const URL = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyAkO9HntmqeRtYEwNj_7qMd6c4yusGdWDY&cx=011835907953613196564:eyxj-nvckro&q=:1';
+const URL = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCNWIjYtQeEU-LjLWnRYa28GT7G91b7hL8&cx=011835907953613196564:eyxj-nvckro&q=:1';
 class WebSearch {
   async retrieveResults(query) {
     const url = URL.replace(':1', query);
